35b6f0b1bf438e4874ee73bf432f99e828039a67
deenap <deena.earth@gmail.com>
2016-06-18 18:16:35 +0530

    added test scripts and suite

---
+++ src/main/java/testcases/TestScript1.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import org.junit.Test;
+
+public class TestScript1 {
+	
+	@Test
+	public void test_case1(){
+		System.out.println("This is testcase1");
+	}
+	
+	@Test
+	public void test_case2(){
+		System.out.println("This is testcase2");
+	}
+
+}
